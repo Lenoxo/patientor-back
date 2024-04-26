4534e593933d6c02b4afb4adaa0f3f822818136a
Emanuel Padilla <emanuehl159@gmail.com>
2024-04-25 22:27:55 -0500

    Fix cors problem

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ const app = express();
 const PORT = 8080;
 import diagnosesRouter from "./routes/diagnoses";
 import patientsRouter from "./routes/patients";
+import cors from "cors";
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/ping", (_req, res) => {
